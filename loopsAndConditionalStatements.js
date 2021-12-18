d0b5d16b7d3097ecff67e256ee10c0e5059b6438
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2021-12-18 14:33:01 +0200

    Loops and Conditional Statements
    const flag = true;
    
    if(!flag){
    }
    else{
    }
    
    let a = 0;
    do{
    a++;
    }while(a>5);
    console.log(a)

---
+++ loopsAndConditionalStatements.js
@@ -0,0 +1,28 @@
+const flag = true;
+
+if(!flag){
+    console.log(typeof(flag))
+    console.log("condition is satisfied")
+}
+else {
+    console.log(typeof(flag))
+    console.log(!flag)
+    console.log("condition is not satisfied")
+}
+// let i = 0;
+// while(i<2){
+//     i++;
+//     console.log(i)
+// }
+
+// let i = 0;
+// while (i>10){
+//     i++;
+//     console.log(i)
+// }
+
+let i = 0;
+do{
+    i++
+}while(i>1);
+    console.log(i)
