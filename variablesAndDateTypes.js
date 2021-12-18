f6d9fbfabf9e99cf7c40c77c649c51672a02024b
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2021-12-18 14:30:15 +0200

    Variables and Data Types
    var vs let vs const
    
    console.log(a);
    console.log(typeof(a);
    
    required
    null
    undefined

---
+++ variablesAndDateTypes.js
@@ -0,0 +1,44 @@
+console.log("Hello World")
+
+let a = 4
+console.log(a)
+console.log(typeof(a));
+
+let b = 234.6
+console.log(typeof(b));
+
+let c = "Lukas da man";
+console.log(typeof(c));
+
+let required = true;
+console.log(typeof(required));
+
+let d = null;
+console.log(typeof(d));
+
+let e = undefined;
+console.log(typeof(e));
+
+
+console.log(c);
+
+//let c = a + b
+//we can not redeclare variable with let keyword but possible with var
+c = a + b;
+//reassigning is allowed with let
+//var c = a + b is also allowed
+console.log(c);
+
+console.log(!required);
+
+//var
+//let
+//const
+
+//these are comments
+/*
+my commentsd
+are too long
+to fit in
+one line
+*/
