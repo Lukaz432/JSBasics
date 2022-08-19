29416c52c4590992b9b2f00294714321bd94f697
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2022-08-19 20:18:20 +0300

    Add files via upload
    object class
    creating an object for a class
    class properties
    methods as properties
    constructor
    instance variables

---
+++ oopsClassesAndMethods.js
@@ -0,0 +1,29 @@
+class Person
+{
+    age = 25
+    //calling a class property
+    get location() //location = 'Canada'
+    {
+        return 'Canada'
+    }
+//constructor is a method which executes by default when youn create object of the chosen class
+constructor(firstName, lastName)
+{
+    //values depend on the object we call which is why these are called instance variables
+    this.firstName = firstName
+    this.lastName = lastName
+}
+//methods
+fullName()
+{
+    console.log(this.firstName+this.lastName)
+}
+}
+
+//creating an object for a class
+let person = new Person('Tim', 'Joseph')
+let person1 = new Person('John', 'Abruzzi')
+console.log(person.age)
+console.log(person.location)
+console.log(person.fullName())
+console.log(person1.fullName())
